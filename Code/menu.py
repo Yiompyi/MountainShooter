9e77c1951169c7e577bc1749cc7db11294d9cc54
Yiompyi <Yiompyi@gmail.com>
2024-08-31 10:26:36 -0300

    Creating options in the menu Updated constants

--- Code/menu.py
+++ Code/menu.py
@@ -3,7 +3,7 @@
 import pygame.image
 from pygame.font import Font
 
-from Code.const import WIN_WIDTH, COLOR_ORANGE
+from Code.const import WIN_WIDTH, COLOR_ORANGE, COLOR_WHITE, MENU_OPTION
 
 
 class Menu:
@@ -13,6 +13,7 @@ class Menu:
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
+        menu_option = 1
         pygame.mixer_music.load('./asset/Menu.mp3')
         pygame.mixer.music.play(-1)
         while True:
@@ -20,12 +21,21 @@ class Menu:
             self.window.blit(source=self.surf, dest=self.rect)
             self.menu_text(50, "Mountain", (COLOR_ORANGE), ((WIN_WIDTH / 2), 70))
             self.menu_text(50, "Shooter", (COLOR_ORANGE), ((WIN_WIDTH / 2), 120))
-            pygame.display.flip()
+
+            for i in range(len(MENU_OPTION)):
+                if i == menu_option:
+                    self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
+                else:
+                    self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2 ), 200 + 25 * i))
+
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()  # Close window
                     quit()  # End pygame
+            else:
+
+                pygame.display.flip()
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont(name='Lucida Sans Typewriter', size=text_size)
